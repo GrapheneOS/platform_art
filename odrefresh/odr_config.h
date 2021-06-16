hopez13/art

odrefresh: Support option to run dex2oat in PVM
pvm_exec is an executable to run a command remotely, assuming the code
is also in the VM. Since the input files may not be available in the VM,
pvm_exec internally takes over the FDs and becomes a file server. In the
remote execution setup, dex2oat receives the same FD numbers, and relies
on "authfs", a FUSE client that talkes to the file server, to translate
the read/write.

By wrapping dex2oat with pvm_exec, this change makes it possible run
dex2oat remotely, though not necessarily securely. For example, a
command is not a secure interface since it allows unlimited numbers of
combination of input paramters (or even allow manipulating the
compilation, e.g. by adjusting Java property).

We'll need to change the interface later. But in the meantime, this
change will allow us to iterate more easily.

Bug: 187346876
Test: 1. Start a PVM with ART and CompOS apex mounted
      2. /apex/com.android.art/bin/odrefresh --use-compos=$CID \
         --force-compile
      # odrefresh returns successfully (though much slower right now)
Change-Id: I722e5290246e180a89f6d42d9a6bc10c79dc0d1a

odrefresh/odr_config.h
@@ -54,6 +54,7 @@ class OdrConfig final {
   std::string system_server_classpath_;
   std::string updatable_bcp_packages_file_;
   ZygoteKind zygote_kind_;
+  std::string cid_;
 
  public:
   explicit OdrConfig(const char* program_name)
@@ -117,6 +118,8 @@ class OdrConfig final {
   bool GetDryRun() const { return dry_run_; }
   const std::string& GetSystemServerClasspath() const { return system_server_classpath_; }
   const std::string& GetUpdatableBcpPackagesFile() const { return updatable_bcp_packages_file_; }
+  bool UseCompilationOs() const { return !cid_.empty(); }
+  std::string GetCompilationOsAddress() const { return cid_; }
 
   void SetApexInfoListFile(const std::string& file_path) { apex_info_list_file_ = file_path; }
   void SetArtBinDir(const std::string& art_bin_dir) { art_bin_dir_ = art_bin_dir; }
@@ -127,6 +130,7 @@ class OdrConfig final {
 
   void SetDryRun() { dry_run_ = true; }
   void SetIsa(const InstructionSet isa) { isa_ = isa; }
+  void SetCompilationOsAddress(const std::string& cid) { cid_ = cid; }
 
   void SetSystemServerClasspath(const std::string& classpath) {
     system_server_classpath_ = classpath;

GrapheneOS/platform_art

odrefresh/odr_config.h
@@ -54,6 +54,7 @@ class OdrConfig final {
   std::string system_server_classpath_;
   std::string updatable_bcp_packages_file_;
   ZygoteKind zygote_kind_;
+  std::string compilation_os_address_;
 
  public:
   explicit OdrConfig(const char* program_name)
@@ -117,6 +118,8 @@ class OdrConfig final {
   bool GetDryRun() const { return dry_run_; }
   const std::string& GetSystemServerClasspath() const { return system_server_classpath_; }
   const std::string& GetUpdatableBcpPackagesFile() const { return updatable_bcp_packages_file_; }
+  bool UseCompilationOs() const { return !compilation_os_address_.empty(); }
+  std::string GetCompilationOsAddress() const { return compilation_os_address_; }
 
   void SetApexInfoListFile(const std::string& file_path) { apex_info_list_file_ = file_path; }
   void SetArtBinDir(const std::string& art_bin_dir) { art_bin_dir_ = art_bin_dir; }
@@ -127,6 +130,7 @@ class OdrConfig final {
 
   void SetDryRun() { dry_run_ = true; }
   void SetIsa(const InstructionSet isa) { isa_ = isa; }
+  void SetCompilationOsAddress(const std::string& address) { compilation_os_address_ = address; }
 
   void SetSystemServerClasspath(const std::string& classpath) {
     system_server_classpath_ = classpath;