hopez13/art

Add tests for the artd Binder interface.
This CL adds a test to make sure the artd binder interface can be
found and called.

Test: m ArtdIntegrationTests
Bug: 177273468
Change-Id: Ie456cb43ad6320bc4ce206d1f8e7948622491dff

artd/tests/src/com/android/art/ArtdIntegrationTests.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.art;
+
+import android.os.IArtd;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+
+// import androidx.test.InstrumentationRegistry;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.lang.IllegalStateException;
+
+/**
+ * Integration tests for Artd.
+ *
+ * Run with "atest ArtdIntegrationTests".
+ */
+@RunWith(JUnit4.class)
+public final class ArtdIntegrationTests {
+
+    private static final String TAG = "ArtdTests";
+
+    private static IArtd mArtd;
+
+    @Before
+    public void setUp() {
+        IBinder rawBinderService = ServiceManager.getService("artd");
+        if (rawBinderService == null) {
+            throw new IllegalStateException("Unable to fetch artd service from binder.");
+        }
+
+        mArtd = IArtd.Stub.asInterface(rawBinderService);
+    }
+
+    // @After
+    // public void tearDown() {
+    //     ;
+    // }
+
+    // Test basic build and publish functionality for Artd.
+    @Test
+    public void testBuild() throws RemoteException {
+        Assert.assertTrue(mArtd.buildTest());
+    }
+}

GrapheneOS/platform_art

artd/tests/src/com/android/art/ArtdIntegrationTests.java
@@ -1,0 +1,60 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.art;
+
+import android.os.IArtd;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.lang.IllegalStateException;
+
+/**
+ * Integration tests for Artd.
+ *
+ * Run with "atest ArtdIntegrationTests".
+ */
+@RunWith(JUnit4.class)
+public final class ArtdIntegrationTests {
+
+    private static final String TAG = "ArtdTests";
+
+    private static IArtd mArtd;
+
+    @Before
+    public void setUp() {
+        IBinder rawBinderService = ServiceManager.getService("artd");
+        if (rawBinderService == null) {
+            throw new IllegalStateException("Unable to fetch artd service from binder.");
+        }
+
+        mArtd = IArtd.Stub.asInterface(rawBinderService);
+    }
+
+    // Test basic build and publish functionality for Artd.
+    @Test
+    public void testLiveness() throws RemoteException {
+        Assert.assertTrue(mArtd.isAlive());
+    }
+}