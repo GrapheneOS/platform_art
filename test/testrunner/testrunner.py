hopez13/art

ART: Implements a test option of forced generation of .cfg
This patch introduces the option to dump .cfg for any type of test. If
the "--dump-cfg <path/graph.cfg> option" is present, then it generates
a .cfg following the same logic used for the "checker" tests but
forcing it for any type of test. This option works for both host and
target.

Before the artifacts are wiped, the generated .cfg file is copied to
the specified host path.

Note that it will only dump a .cfg if the compiler type is set to
"optimizing". Otherwise, it will throw a warning.

Test: ./art/test/run-test  --host --simulate-arm64  --optimizing \
--dump-cfg <path>/graph.cfg  <test>

Change-Id: I034b13b401bd1dee8d871297da8d09e807a35fd1

test/testrunner/testrunner.py
@@ -137,6 +137,7 @@ build = False
 dist = False
 gdb = False
 gdb_arg = ''
+dump_cfg = ''
 csv_result = None
 csv_writer = None
 runtime_option = ''
@@ -419,6 +420,9 @@ def run_tests(tests):
     options_all += ' --gdb'
     if gdb_arg:
       options_all += ' --gdb-arg ' + gdb_arg
+
+  if dump_cfg:
+    options_all += ' --dump-cfg ' + dump_cfg
 
   options_all += ' ' + ' '.join(run_test_option)
 
@@ -1102,6 +1106,7 @@ def parse_option():
   global dist
   global gdb
   global gdb_arg
+  global dump_cfg
   global runtime_option
   global run_test_option
   global timeout
@@ -1143,6 +1148,9 @@ def parse_option():
   global_group.set_defaults(build = env.ART_TEST_RUN_TEST_BUILD)
   global_group.add_argument('--gdb', action='store_true', dest='gdb')
   global_group.add_argument('--gdb-arg', dest='gdb_arg')
+  global_group.add_argument('--dump-cfg', dest='dump_cfg',
+                            help="""Dump the CFG to the specific host path.
+                            Example \"--dump-cfg <full-path>/graph.cfg\".""")
   global_group.add_argument('--run-test-option', action='append', dest='run_test_option',
                             default=[],
                             help="""Pass an option, unaltered, to the run-test script.
@@ -1216,6 +1224,8 @@ def parse_option():
     gdb = True
     if options['gdb_arg']:
       gdb_arg = options['gdb_arg']
+  if options['dump_cfg']:
+    dump_cfg = options['dump_cfg']
   runtime_option = options['runtime_option'];
   with_agent = options['with_agent'];
   run_test_option = sum(map(shlex.split, options['run_test_option']), [])

GrapheneOS/platform_art

test/testrunner/testrunner.py
@@ -137,6 +137,7 @@ build = False
 dist = False
 gdb = False
 gdb_arg = ''
+dump_cfg = ''
 csv_result = None
 csv_writer = None
 runtime_option = ''
@@ -419,6 +420,9 @@ def run_tests(tests):
     options_all += ' --gdb'
     if gdb_arg:
       options_all += ' --gdb-arg ' + gdb_arg
+
+  if dump_cfg:
+    options_all += ' --dump-cfg ' + dump_cfg
 
   options_all += ' ' + ' '.join(run_test_option)
 
@@ -1102,6 +1106,7 @@ def parse_option():
   global dist
   global gdb
   global gdb_arg
+  global dump_cfg
   global runtime_option
   global run_test_option
   global timeout
@@ -1143,6 +1148,9 @@ def parse_option():
   global_group.set_defaults(build = env.ART_TEST_RUN_TEST_BUILD)
   global_group.add_argument('--gdb', action='store_true', dest='gdb')
   global_group.add_argument('--gdb-arg', dest='gdb_arg')
+  global_group.add_argument('--dump-cfg', dest='dump_cfg',
+                            help="""Dump the CFG to the specified host path.
+                            Example \"--dump-cfg <full-path>/graph.cfg\".""")
   global_group.add_argument('--run-test-option', action='append', dest='run_test_option',
                             default=[],
                             help="""Pass an option, unaltered, to the run-test script.
@@ -1216,6 +1224,8 @@ def parse_option():
     gdb = True
     if options['gdb_arg']:
       gdb_arg = options['gdb_arg']
+  if options['dump_cfg']:
+    dump_cfg = options['dump_cfg']
   runtime_option = options['runtime_option'];
   with_agent = options['with_agent'];
   run_test_option = sum(map(shlex.split, options['run_test_option']), [])