hopez13/art

Allow odrefresh to write elsewhere than dalvik-cache
For CompOS we want to be able to generate artifacts in different
subdirectories, e.g. /data/misc/apexdata/com.android.art/pending
rather than /data/misc/apexdata/com.android.art/dalvik-cache.

Add a command line flag to enable this.

Bug: 200020887
Test: atest art_standalone_libartbase_tests
Test: manual: odrefresh --dalvik-cache=foo --force-compile
Change-Id: Id10c9bcc8cb9f810c48727abc1858f5b16b360d2

libartbase/base/file_utils.cc
@@ -370,11 +370,18 @@ std::string GetBootImagePath(bool on_system, const std::string& jar_path) {
   }
 }
 
+static /*constinit*/ std::string_view dalvik_cache_sub_dir = "dalvik-cache";
+
+void OverrideDalvikCacheSubDirectory(std::string sub_dir) {
+    static std::string overriden_dalvik_cache_sub_dir;
+    overriden_dalvik_cache_sub_dir = std::move(sub_dir);
+    dalvik_cache_sub_dir = overriden_dalvik_cache_sub_dir;
+}
+
 static std::string GetDalvikCacheDirectory(std::string_view root_directory,
                                            std::string_view sub_directory = {}) {
-  static constexpr std::string_view kDalvikCache = "dalvik-cache";
   std::stringstream oss;
-  oss << root_directory << '/' << kDalvikCache;
+  oss << root_directory << '/' << dalvik_cache_sub_dir;
   if (!sub_directory.empty()) {
     oss << '/' << sub_directory;
   }

GrapheneOS/platform_art

libartbase/base/file_utils.cc
@@ -370,11 +370,18 @@ std::string GetBootImagePath(bool on_system, const std::string& jar_path) {
   }
 }
 
+static /*constinit*/ std::string_view dalvik_cache_sub_dir = "dalvik-cache";
+
+void OverrideDalvikCacheSubDirectory(std::string sub_dir) {
+    static std::string overridden_dalvik_cache_sub_dir;
+    overridden_dalvik_cache_sub_dir = std::move(sub_dir);
+    dalvik_cache_sub_dir = overridden_dalvik_cache_sub_dir;
+}
+
 static std::string GetDalvikCacheDirectory(std::string_view root_directory,
                                            std::string_view sub_directory = {}) {
-  static constexpr std::string_view kDalvikCache = "dalvik-cache";
   std::stringstream oss;
-  oss << root_directory << '/' << kDalvikCache;
+  oss << root_directory << '/' << dalvik_cache_sub_dir;
   if (!sub_directory.empty()) {
     oss << '/' << sub_directory;
   }