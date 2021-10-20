hopez13/art

Use the .dm file at runtime for verification.
Bug: 112284845
Test: 674-HelloWorld-Dm
Change-Id: Icd07f86cfb2b5428186a4c086f042890eaad249b

runtime/class_linker.cc
@@ -4752,6 +4752,7 @@ bool ClassLinker::VerifyClassUsingOatFile(Thread* self,
   if (oat_file != nullptr) {
     ClassStatus vdex_status = oat_file->GetVdexFile()->ComputeClassStatus(self, klass);
     if (vdex_status >= ClassStatus::kVerifiedNeedsAccessChecks) {
+      VLOG(verifier) << "Vdex verification succes for " << klass->PrettyClass();
       oat_file_class_status = vdex_status;
       return true;
     }

GrapheneOS/platform_art

runtime/class_linker.cc
@@ -4740,6 +4740,7 @@ bool ClassLinker::VerifyClassUsingOatFile(Thread* self,
   if (oat_file != nullptr) {
     ClassStatus vdex_status = oat_file->GetVdexFile()->ComputeClassStatus(self, klass);
     if (vdex_status >= ClassStatus::kVerifiedNeedsAccessChecks) {
+      VLOG(verifier) << "Vdex verification success for " << klass->PrettyClass();
       oat_file_class_status = vdex_status;
       return true;
     }