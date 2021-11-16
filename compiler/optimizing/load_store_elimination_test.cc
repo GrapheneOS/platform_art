3cb3670d162285c6e4304f1f9f85f7c3ff78150d
Nicolas Geoffray <ngeoffray@google.com>
2021-11-16 15:12:04 +0000

    Run LSE with LoadStoreAnalysisType::kBasic analysis.
    Otherwise, the code assumes partial LSE gets run, which isn't the case
    as it's currently disabled.
    
    Test: 834-lse
    Bug: 205813546
    Bug: 205676807
    Merged-In: Ief5cba2964d60522e747395b60f90b862fcfe560
    (cherry picked from commit 2498d855f80171d825ce1b98428cb05c8a504f84)
    Change-Id: I334e1be1f1270fcb25a32e62c2900970d11bfcde

--- compiler/optimizing/load_store_elimination_test.cc
+++ compiler/optimizing/load_store_elimination_test.cc
@@ -4532,7 +4532,8 @@ TEST_F(LoadStoreEliminationTest, PartialLoadPreserved3) {
 // // DO NOT ELIMINATE
 // return obj.field;
 // EXIT
-TEST_F(LoadStoreEliminationTest, PartialLoadPreserved4) {
+// Disabled due to b/205813546.
+TEST_F(LoadStoreEliminationTest, DISABLED_PartialLoadPreserved4) {
   ScopedObjectAccess soa(Thread::Current());
   VariableSizedHandleScope vshs(soa.Self());
   CreateGraph(&vshs);
@@ -4724,7 +4725,8 @@ TEST_F(LoadStoreEliminationTest, PartialLoadPreserved5) {
 // EXIT
 // ELIMINATE
 // return obj.field
-TEST_F(LoadStoreEliminationTest, PartialLoadPreserved6) {
+// Disabled due to b/205813546.
+TEST_F(LoadStoreEliminationTest, DISABLED_PartialLoadPreserved6) {
   CreateGraph();
   AdjacencyListGraph blks(SetupFromAdjacencyList("entry",
                                                  "exit",
