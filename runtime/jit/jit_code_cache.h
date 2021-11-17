hopez13/art

Support calling entry / exit hooks from JIT code for non-debuggable
This CL extends the support to call entry / exit hooks directly
from JITed code for non-debuggable mode. This is done as a best effort
and we won't call method exit hooks for the methods that are already
on the stack when method tracing is enabled. This means it might
be less precise than the existing approach.

The idea is to basically invalidate all the JITed code when we
need method entry / exit hooks and re-JIT them with the support
to call entry / exit hooks as and when necessary.

Test: art/testrunner.py
Change-Id: I79e4547368691a21b26226508228d890369823e9

runtime/jit/jit_code_cache.h
@@ -266,7 +266,8 @@ class JitCodeCache {
               bool is_full_debug_info,
               CompilationKind compilation_kind,
               bool has_should_deoptimize_flag,
-              const ArenaSet<ArtMethod*>& cha_single_implementation_list)
+              const ArenaSet<ArtMethod*>& cha_single_implementation_list,
+              bool has_instrumentation_support)
       REQUIRES_SHARED(Locks::mutator_lock_)
       REQUIRES(!Locks::jit_lock_);
 
@@ -330,7 +331,13 @@ class JitCodeCache {
       REQUIRES(!Locks::jit_lock_)
       REQUIRES_SHARED(Locks::mutator_lock_);
 
+  void InvalidateAllCompiledCodeWithLock()
+      REQUIRES(Locks::jit_lock_)
+      REQUIRES_SHARED(Locks::mutator_lock_);
   void InvalidateAllCompiledCode()
+      REQUIRES(!Locks::jit_lock_)
+      REQUIRES_SHARED(Locks::mutator_lock_);
+  void InvalidateAllCompiledCodeAndUpdateInstrumentation(bool needs_instrumentation_support)
       REQUIRES(!Locks::jit_lock_)
       REQUIRES_SHARED(Locks::mutator_lock_);
 
@@ -513,6 +520,9 @@ class JitCodeCache {
 
   // Condition to wait on for accessing inline caches.
   ConditionVariable inline_cache_cond_ GUARDED_BY(Locks::jit_lock_);
+
+  // Specifies if instrumentation support is required by JITed code.
+  bool needs_instrumentation_support_ GUARDED_BY(Locks::jit_lock_);
 
   // -------------- JIT memory regions ------------------------------------- //
 

GrapheneOS/platform_art

runtime/jit/jit_code_cache.h
@@ -266,7 +266,8 @@ class JitCodeCache {
               bool is_full_debug_info,
               CompilationKind compilation_kind,
               bool has_should_deoptimize_flag,
-              const ArenaSet<ArtMethod*>& cha_single_implementation_list)
+              const ArenaSet<ArtMethod*>& cha_single_implementation_list,
+              bool has_instrumentation_support)
       REQUIRES_SHARED(Locks::mutator_lock_)
       REQUIRES(!Locks::jit_lock_);
 
@@ -330,7 +331,13 @@ class JitCodeCache {
       REQUIRES(!Locks::jit_lock_)
       REQUIRES_SHARED(Locks::mutator_lock_);
 
+  void InvalidateAllCompiledCodeWithLock()
+      REQUIRES(Locks::jit_lock_)
+      REQUIRES_SHARED(Locks::mutator_lock_);
   void InvalidateAllCompiledCode()
+      REQUIRES(!Locks::jit_lock_)
+      REQUIRES_SHARED(Locks::mutator_lock_);
+  void InvalidateAllCompiledCodeForInstrumentation(bool needs_instrumentation_support)
       REQUIRES(!Locks::jit_lock_)
       REQUIRES_SHARED(Locks::mutator_lock_);
 
@@ -513,6 +520,9 @@ class JitCodeCache {
 
   // Condition to wait on for accessing inline caches.
   ConditionVariable inline_cache_cond_ GUARDED_BY(Locks::jit_lock_);
+
+  // Specifies if instrumentation support is required by JITed code.
+  bool needs_instrumentation_support_ GUARDED_BY(Locks::jit_lock_);
 
   // -------------- JIT memory regions ------------------------------------- //
 