hopez13/art

LSE fix removing observable stores with throwing instuctions
Throwing instructions can make some stores observable that
wouldn't be observable othwerwise.

Test: art/test/testrunner/testrunner.py --host --64 --optimizing -b
Bug: 229706824, 227283233
Change-Id: I03683184db0a2b8c42623fc1148efbec8f5cae11

test/530-checker-lse/src/Main.java
@@ -1021,13 +1021,18 @@ public class Main {
 
   /// CHECK-START: int Main.test33(TestClass, boolean) load_store_elimination (after)
   /// CHECK-DAG:                     InstanceFieldSet
-  /// CHECK-DAG:                     Phi
+  /// CHECK-DAG:                     InstanceFieldSet
+  /// CHECK-DAG: <<Phi:i\d+>>        Phi
 
   /// CHECK-START: int Main.test33(TestClass, boolean) load_store_elimination (after)
   /// CHECK:                         InstanceFieldSet
+  /// CHECK:                         InstanceFieldSet
   /// CHECK-NOT:                     InstanceFieldSet
 
-  // Test eliminating non-observable stores.
+  // Test that we are not eliminating the if/else sets to `obj.i`. We have `NullCheck`s on `obj`
+  // when doing `obj.i`. Since `NullCheck` can throw, we save the stores.
+  // The 3rd `obj.i` set is redundant and can be eliminated. It will have the same value and it is
+  // not needed.
   static int test33(TestClass obj, boolean x) {
     int phi;
     if (x) {
@@ -1215,6 +1220,44 @@ public class Main {
       obj.next = new SubTestClass();
     }
     return obj.next.i;
+  }
+
+  private static int test40() {
+    int[] array = new int[1];
+    try {
+      $noinline$fillArrayTest40(array, 100, 0);
+      System.out.println("UNREACHABLE");
+    } catch (Throwable expected) {
+    }
+    assertIntEquals(array[0], 1);
+    try {
+      $noinline$fillArrayTest40(array, 100, 1);
+      System.out.println("UNREACHABLE");
+    } catch (Throwable expected) {
+    }
+    assertIntEquals(array[0], 2);
+    $noinline$fillArrayTest40(array, 100, 2);
+    assertIntEquals(array[0], 150);
+    return array[0];
+  }
+
+  /// CHECK-START: void Main.$noinline$fillArrayTest40(int[], int, int) load_store_elimination (before)
+  /// CHECK:                     ArraySet
+  /// CHECK:                     ArraySet
+  /// CHECK:                     ArraySet
+
+  /// CHECK-START: void Main.$noinline$fillArrayTest40(int[], int, int) load_store_elimination (after)
+  /// CHECK:                     ArraySet
+  /// CHECK:                     ArraySet
+  /// CHECK:                     ArraySet
+
+  // Check that the stores to array[0] are not eliminated since we can throw in between the stores.
+  private static void $noinline$fillArrayTest40(int[] array, int a, int b) {
+    array[0] = 1;
+    int x = a / b;
+    array[0] = 2;
+    int y = a / (b - 1);
+    array[0] = x + y;
   }
 
   /// CHECK-START: int Main.$noinline$testConversion1(TestClass, int) load_store_elimination (before)
@@ -1655,12 +1698,13 @@ public class Main {
   /// CHECK: InstanceFieldSet
 
   /// CHECK-START: int Main.testStoreStore6(TestClass2, TestClass2) load_store_elimination (after)
-  /// CHECK-NOT: InstanceFieldSet
+  /// CHECK: InstanceFieldSet
   /// CHECK: InstanceFieldGet
   /// CHECK: InstanceFieldSet
 
   private static int testStoreStore6(TestClass2 obj1, TestClass2 obj2) {
-    obj1.i = 81;      // This store is not needed since obj2.j cannot load from it.
+    obj1.i = 81; // Even though the value in `obj1.i` will be overriden below, this store is needed
+                 // since obj2.j has a NullCheck and can throw.
     int j = obj2.j;
     obj1.i = 82;
     return j;
@@ -4116,6 +4160,7 @@ public class Main {
     assertIntEquals(test38(new TestClass(), false), 2);
     assertIntEquals(test39(new TestClass(), true), 0);
     assertIntEquals(test39(new TestClass(), false), 1);
+    assertIntEquals(test40(), 150);
 
     testFinalizableByForcingGc();
     testFinalizableWithLoopByForcingGc();

GrapheneOS/platform_art

(cherry picked from commit ea24a14ff37c708360b4cb38baba82af0931d451)
Merged-In: I03683184db0a2b8c42623fc1148efbec8f5cae11

test/530-checker-lse/src/Main.java
@@ -1021,13 +1021,18 @@ public class Main {
 
   /// CHECK-START: int Main.test33(TestClass, boolean) load_store_elimination (after)
   /// CHECK-DAG:                     InstanceFieldSet
-  /// CHECK-DAG:                     Phi
+  /// CHECK-DAG:                     InstanceFieldSet
+  /// CHECK-DAG: <<Phi:i\d+>>        Phi
 
   /// CHECK-START: int Main.test33(TestClass, boolean) load_store_elimination (after)
   /// CHECK:                         InstanceFieldSet
+  /// CHECK:                         InstanceFieldSet
   /// CHECK-NOT:                     InstanceFieldSet
 
-  // Test eliminating non-observable stores.
+  // Test that we are not eliminating the if/else sets to `obj.i`. We have `NullCheck`s on `obj`
+  // when doing `obj.i`. Since `NullCheck` can throw, we save the stores.
+  // The 3rd `obj.i` set is redundant and can be eliminated. It will have the same value and it is
+  // not needed.
   static int test33(TestClass obj, boolean x) {
     int phi;
     if (x) {
@@ -1215,6 +1220,48 @@ public class Main {
       obj.next = new SubTestClass();
     }
     return obj.next.i;
+  }
+
+  private static int test40() {
+    int[] array = new int[1];
+    try {
+      $noinline$fillArrayTest40(array, 100, 0);
+      System.out.println("UNREACHABLE");
+    } catch (Throwable expected) {
+    }
+    assertIntEquals(array[0], 1);
+    try {
+      $noinline$fillArrayTest40(array, 100, 1);
+      System.out.println("UNREACHABLE");
+    } catch (Throwable expected) {
+    }
+    assertIntEquals(array[0], 2);
+    $noinline$fillArrayTest40(array, 100, 2);
+    assertIntEquals(array[0], 150);
+    return array[0];
+  }
+
+  /// CHECK-START: void Main.$noinline$fillArrayTest40(int[], int, int) load_store_elimination (before)
+  /// CHECK:                     ArraySet
+  /// CHECK:                     DivZeroCheck
+  /// CHECK:                     ArraySet
+  /// CHECK:                     DivZeroCheck
+  /// CHECK:                     ArraySet
+
+  /// CHECK-START: void Main.$noinline$fillArrayTest40(int[], int, int) load_store_elimination (after)
+  /// CHECK:                     ArraySet
+  /// CHECK:                     DivZeroCheck
+  /// CHECK:                     ArraySet
+  /// CHECK:                     DivZeroCheck
+  /// CHECK:                     ArraySet
+
+  // Check that the stores to array[0] are not eliminated since we can throw in between the stores.
+  private static void $noinline$fillArrayTest40(int[] array, int a, int b) {
+    array[0] = 1;
+    int x = a / b;
+    array[0] = 2;
+    int y = a / (b - 1);
+    array[0] = x + y;
   }
 
   /// CHECK-START: int Main.$noinline$testConversion1(TestClass, int) load_store_elimination (before)
@@ -1655,12 +1702,13 @@ public class Main {
   /// CHECK: InstanceFieldSet
 
   /// CHECK-START: int Main.testStoreStore6(TestClass2, TestClass2) load_store_elimination (after)
-  /// CHECK-NOT: InstanceFieldSet
+  /// CHECK: InstanceFieldSet
   /// CHECK: InstanceFieldGet
   /// CHECK: InstanceFieldSet
 
   private static int testStoreStore6(TestClass2 obj1, TestClass2 obj2) {
-    obj1.i = 81;      // This store is not needed since obj2.j cannot load from it.
+    obj1.i = 81; // Even though the value in `obj1.i` will be overridden below, this store is needed
+                 // since obj2.j has a NullCheck and can throw.
     int j = obj2.j;
     obj1.i = 82;
     return j;
@@ -4116,6 +4164,7 @@ public class Main {
     assertIntEquals(test38(new TestClass(), false), 2);
     assertIntEquals(test39(new TestClass(), true), 0);
     assertIntEquals(test39(new TestClass(), false), 1);
+    assertIntEquals(test40(), 150);
 
     testFinalizableByForcingGc();
     testFinalizableWithLoopByForcingGc();