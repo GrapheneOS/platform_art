a501686ed1841053c54e318ce1ce15f96c484d55
Martin Stjernholm <mast@google.com>
2022-09-11 00:46:57 +0100

    Add tests that try to load non-shared native libs in each partition.
    One test case in each app that tries to load them directly, and another
    one that goes through a shared library in the system partition.
    
    Also set the appropriate Soong properties on the test apps, for what it
    may be worth (doesn't seem to have any effect that we don't get by just
    pushing them to the various partitions).
    
    Test: atest libnativeloader_e2e_tests
    Bug: 137356719
    Bug: 237577392
    Change-Id: I9e2c8162e0f0928020355a507b90f0fa76bf6b6b

--- libnativeloader/test/src/android/test/app/VendorAppTest.java
+++ libnativeloader/test/src/android/test/app/VendorAppTest.java
@@ -17,6 +17,7 @@
 package android.test.app;
 
 import android.test.lib.TestUtils;
+import android.test.systemsharedlib.SystemSharedLib;
 import androidx.test.filters.SmallTest;
 import androidx.test.runner.AndroidJUnit4;
 import org.junit.Test;
@@ -26,12 +27,32 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class VendorAppTest {
     @Test
-    public void testLoadLibraries() {
-        TestUtils.assertLinkerNamespaceError("foo.oem1");
-        TestUtils.assertLinkerNamespaceError("bar.oem1");
-        TestUtils.assertLinkerNamespaceError("foo.oem2");
-        TestUtils.assertLinkerNamespaceError("bar.oem2");
+    public void testLoadExtendedPublicLibraries() {
+        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("foo.oem1"));
+        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("bar.oem1"));
+        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("foo.oem2"));
+        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("bar.oem2"));
         System.loadLibrary("foo.product1");
         System.loadLibrary("bar.product1");
+    }
+
+    @Test
+    public void testLoadPrivateLibraries() {
+        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("system_private1"));
+        TestUtils.assertLibraryNotFound(() -> System.loadLibrary("product_private1"));
+        // TODO(mast): The vendor app fails to load a private vendor library because it gets
+        // classified as untrusted_app in SELinux, which doesn't have access to vendor_file. Even an
+        // app in /vendor/priv-app, which gets classified as priv_app, still doesn't have access to
+        // vendor_file. Check that the test setup is correct and if this is WAI.
+        TestUtils.assertLibraryNotFound(() -> System.loadLibrary("vendor_private1"));
+    }
+
+    @Test
+    public void testLoadPrivateLibrariesViaSystemSharedLib() {
+        // TODO(b/237577392): Fix this use case.
+        TestUtils.assertLinkerNamespaceError(() -> SystemSharedLib.loadLibrary("system_private2"));
+
+        TestUtils.assertLibraryNotFound(() -> SystemSharedLib.loadLibrary("product_private2"));
+        TestUtils.assertLibraryNotFound(() -> SystemSharedLib.loadLibrary("vendor_private2"));
     }
 }
