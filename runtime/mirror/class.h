c998f36995d652f873dbbc74935dc2375b12cd39
Sorin Basca <sorinbasca@google.com>
2022-09-12 14:49:25 +0100

    Add java.lang.Record from upstream
    Test: m
    
    Change-Id: Ifd42ac0e77b06d3d2a3cd9b08e4b735c4402a706

--- runtime/mirror/class.h
+++ runtime/mirror/class.h
@@ -566,7 +566,7 @@ class MANAGED Class final : public Object {
   // The size of java.lang.Class.class.
   static uint32_t ClassClassSize(PointerSize pointer_size) {
     // The number of vtable entries in java.lang.Class.
-    uint32_t vtable_entries = Object::kVTableLength + 73;
+    uint32_t vtable_entries = Object::kVTableLength + 78;
     return ComputeClassSize(true, vtable_entries, 0, 0, 4, 1, 0, pointer_size);
   }
 
