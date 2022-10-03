9636d6fde8252ef691ff6ad18c53be610b75d72a
Stefano Cianciulli <scianciulli@google.com>
2022-10-03 14:09:05 +0000

    Report Odrefresh compilation status, exit code and signal to statsd
    As part of the changes done in aosp/2223024, we are
    now collecting some additional information about the
    compilation for primary/secondary architecture and
    system_server in dex2oat:
     - result status
     - exit code (if status is Exited)
     - signal (if status is Signaled)
    
    This CL reports this new data points to statsd as part
    of the already existing OdrefreshReported atom, so that
    this new information can be extracted and analysed.
    
    Bug: 246534524
    Test: atest ArtGtestsTargetChroot (in particular art_standalone_odrefresh_tests)
    Change-Id: I9705845a6bae0716d091400adde6415c20eda3de
    (cherry picked from commit 6eb814ce3a0d53126f066cb2148be69a4679dd7a)
    Merged-In: I9705845a6bae0716d091400adde6415c20eda3de

--- runtime/exec_utils.h
+++ runtime/exec_utils.h
@@ -44,6 +44,8 @@ struct ExecCallbacks {
 };
 
 struct ExecResult {
+  // This struct needs to be in sync with the ExecResultStatus enum contained within
+  // the OdrefreshReported atom in frameworks/proto_logging/atoms.proto.
   enum Status {
     // Unable to get the status.
     kUnknown = 0,
@@ -55,6 +57,7 @@ struct ExecResult {
     kTimedOut = 3,
     // Failed to start the process.
     kStartFailed = 4,
+    kLast = kStartFailed
   };
 
   Status status = kUnknown;
