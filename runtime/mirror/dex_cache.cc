cc97f11fe689c1344bb04ab85e8bdc7baaeb3fb1
Nicolas Geoffray <ngeoffray@google.com>
2022-10-26 08:00:23 +0000

    Revert "Allocate dex cache arrays at startup."
    This reverts commit a25aeadee55630760b521176d0813d12eebae8c8.
    
    Bug: 255465158
    
    Reason for revert:b/255465158
    
    Change-Id: Ic9c89532bfec75d2706b8dbcc599fad5c4c71735

--- runtime/mirror/dex_cache.cc
+++ runtime/mirror/dex_cache.cc
@@ -165,21 +165,5 @@ ObjPtr<ClassLoader> DexCache::GetClassLoader() {
   return GetFieldObject<ClassLoader>(OFFSET_OF_OBJECT_MEMBER(DexCache, class_loader_));
 }
 
-bool DexCache::AtStartup() {
-  return !Runtime::Current()->GetStartupCompleted();
-}
-
-void DexCache::UnlinkStartupCaches() {
-  if (GetDexFile() == nullptr) {
-    // Unused dex cache.
-    return;
-  }
-  UnlinkStringsArrayIfStartup();
-  UnlinkResolvedFieldsArrayIfStartup();
-  UnlinkResolvedMethodsArrayIfStartup();
-  UnlinkResolvedTypesArrayIfStartup();
-  UnlinkResolvedMethodTypesArrayIfStartup();
-}
-
 }  // namespace mirror
 }  // namespace art
