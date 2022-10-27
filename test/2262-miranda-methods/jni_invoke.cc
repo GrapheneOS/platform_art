6de870da9635b68d5cc1c170d61308edb63d845d
Mythri Alle <mythria@google.com>
2022-10-27 18:34:46 +0000

    Canonicalize non default conflicting methods in jni::EncodeArtMethod
    jmethodIDs returned by jni::EncodeArtMethod are used by external
    interfaces (for ex: jvmti) to match methods. As an example, when single
    stepping jvmti returns method id corresponding to the methods on the
    stack. To map these method ids to method name / signature the method ids
    are matched against the method ids returned by the GetDeclaredMethods of
    the class. This generally works but breaks for copied methods. So we
    should canonicalize methods before returning the method id. We already
    do this when using opaque jni ids.
    
    We shouldn't canonicalize default conflicting methods. Canonicalization
    returns one of the interface methods. If this method id is used to call
    the method via the CallNonVirtualMethod jni interface, the method
    executes the interface method instead of throwing ICCE error.
    
    This CL also fixes this for opaque jni ids to not canonicalize default
    conflict methods.This also adds a few comments for the opaque jni ids.
    
    Bug: 244683447
    Test: art/testrunner.py -t 2243-step-default-method, 2262*
    Change-Id: Ie2e9225fb8ebf5235f08345a079eb73f28f33961

---
+++ test/2262-miranda-methods/jni_invoke.cc
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <jni.h>
+
+#include "jni/java_vm_ext.h"
+#include "runtime.h"
+
+namespace art {
+
+extern "C" JNIEXPORT void JNICALL
+Java_Main_CallNonvirtual(JNIEnv* env, jclass k ATTRIBUTE_UNUSED, jobject o, jclass c, jmethodID m) {
+  env->CallNonvirtualVoidMethod(o, c, m);
+}
+
+}  // namespace art
