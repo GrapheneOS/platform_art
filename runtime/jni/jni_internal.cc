ba2201479824cc278964b76abb340130f7289081
Vladimir Marko <vmarko@google.com>
2022-11-21 15:30:40 +0100

    Rename `kJniTransitionOrInvalid` to `kJniTransition`.
    And remove `IndirectReferenceTable::SynchronizedGet()`.
    
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing
    Bug: 172332525
    Change-Id: Ice599b33f4d494fccc0efd3b3a3574e930088f89

--- runtime/jni/jni_internal.cc
+++ runtime/jni/jni_internal.cc
@@ -2854,7 +2854,7 @@ class JNI {
       return JNIGlobalRefType;
     case kWeakGlobal:
       return JNIWeakGlobalRefType;
-    case kJniTransitionOrInvalid:
+    case kJniTransition:
       // Assume value is in a JNI transition frame.
       return JNILocalRefType;
     }
