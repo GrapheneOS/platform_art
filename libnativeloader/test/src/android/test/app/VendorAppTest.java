hopez13/art

Test loading of extended public libraries through absolute paths.
Test: atest libnativeloader_e2e_tests
Bug: 258340826
Change-Id: I2759a29d83b04e69bc0678e50eb154d117c1323c

libnativeloader/test/src/android/test/app/VendorAppTest.java
@@ -31,11 +31,11 @@ import org.junit.runner.RunWith;
 public class VendorAppTest {
     @Test
     public void testLoadExtendedPublicLibraries() {
-        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("foo.oem1"));
+        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("foo1.oem1"));
         TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("bar.oem1"));
         TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("foo.oem2"));
         TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("bar.oem2"));
-        System.loadLibrary("foo.product1");
+        System.loadLibrary("foo1.product1");
         System.loadLibrary("bar.product1");
     }
 
@@ -79,6 +79,13 @@ public class VendorAppTest {
                 () -> VendorSharedLib.loadLibrary("systemext_private5"));
         TestUtils.assertLibraryNotFound(() -> VendorSharedLib.loadLibrary("product_private5"));
         VendorSharedLib.loadLibrary("vendor_private5");
+    }
+
+    @Test
+    public void testLoadExtendedPublicLibrariesWithAbsolutePaths() {
+        TestUtils.assertLinkerNamespaceError(
+                () -> System.load(TestUtils.libPath("/system", "foo2.oem1")));
+        System.load(TestUtils.libPath("/product", "foo2.product1"));
     }
 
     @Test

GrapheneOS/platform_art

libnativeloader/test/src/android/test/app/VendorAppTest.java
@@ -31,12 +31,12 @@ import org.junit.runner.RunWith;
 public class VendorAppTest {
     @Test
     public void testLoadExtendedPublicLibraries() {
-        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("foo.oem1"));
-        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("bar.oem1"));
-        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("foo.oem2"));
-        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("bar.oem2"));
-        System.loadLibrary("foo.product1");
-        System.loadLibrary("bar.product1");
+        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("system_extpub.oem1"));
+        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("system_extpub.oem2"));
+        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("system_extpub1.oem1"));
+        TestUtils.assertLinkerNamespaceError(() -> System.loadLibrary("system_extpub_nouses.oem2"));
+        System.loadLibrary("product_extpub.product1");
+        System.loadLibrary("product_extpub1.product1");
     }
 
     @Test
@@ -79,6 +79,13 @@ public class VendorAppTest {
                 () -> VendorSharedLib.loadLibrary("systemext_private5"));
         TestUtils.assertLibraryNotFound(() -> VendorSharedLib.loadLibrary("product_private5"));
         VendorSharedLib.loadLibrary("vendor_private5");
+    }
+
+    @Test
+    public void testLoadExtendedPublicLibrariesWithAbsolutePaths() {
+        TestUtils.assertLinkerNamespaceError(
+                () -> System.load(TestUtils.libPath("/system", "system_extpub2.oem1")));
+        System.load(TestUtils.libPath("/product", "product_extpub2.product1"));
     }
 
     @Test