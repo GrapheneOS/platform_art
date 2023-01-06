ae12f96965dd1fb1cf5fefb0188b749e921ba88b
Nicolas Geoffray <ngeoffray@google.com>
2023-01-06 12:06:13 +0000

    Refactor code aroud method resolution.
    - Make unresolved AOT entrypoints use the tls cache
    - Remove duplicate code
    - Inline method access checks logic in its only use.
    - Fix in ClassLinker::ResolveMethodWithoutInvokeType() by calling shared
      helper.
    
    Test: test.py
    Change-Id: I1f42b5d0ac1dcd9c5eb483db9c5c5eefc9b2f4d1

--- runtime/class_linker.h
+++ runtime/class_linker.h
@@ -367,14 +367,11 @@ class ClassLinker {
       REQUIRES_SHARED(Locks::mutator_lock_)
       REQUIRES(!Locks::dex_lock_, !Roles::uninterruptible_);
 
-  template <InvokeType type, ResolveMode kResolveMode>
-  ArtMethod* GetResolvedMethod(uint32_t method_idx, ArtMethod* referrer)
-      REQUIRES_SHARED(Locks::mutator_lock_);
-
   template <ResolveMode kResolveMode>
   ArtMethod* ResolveMethod(Thread* self, uint32_t method_idx, ArtMethod* referrer, InvokeType type)
       REQUIRES_SHARED(Locks::mutator_lock_)
       REQUIRES(!Locks::dex_lock_, !Roles::uninterruptible_);
+
   ArtMethod* ResolveMethodWithoutInvokeType(uint32_t method_idx,
                                             Handle<mirror::DexCache> dex_cache,
                                             Handle<mirror::ClassLoader> class_loader)
