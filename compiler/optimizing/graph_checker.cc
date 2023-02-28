hopez13/art

Code sinking cleanups
* Reword/refactor sinking throwing instructions
* Removed redundant GetSuccessors().empty() check
* Created a check in GraphCheker regarding no successors
* Update gtest to comply with the new check
* Typo fixes

Test: art/test/testrunner/testrunner.py --host --64 --optimizing -b
Change-Id: Ibf27266f29570a4b1b63bb9bd80e5906207527c0

compiler/optimizing/graph_checker.cc
@@ -319,6 +319,12 @@ void GraphChecker::VisitBasicBlock(HBasicBlock* block) {
     }
   }
 
+  // Ensure all blocks have at least one successor, except the Exit block.
+  if (block->GetSuccessors().empty() && !block->IsExitBlock()) {
+    AddError(StringPrintf("Block %d has no successors and it is not the Exit block.",
+                          block->GetBlockId()));
+  }
+
   // Ensure there is no critical edge (i.e., an edge connecting a
   // block with multiple successors to a block with multiple
   // predecessors). Exceptional edges are synthesized and hence

GrapheneOS/platform_art

compiler/optimizing/graph_checker.cc
@@ -319,6 +319,12 @@ void GraphChecker::VisitBasicBlock(HBasicBlock* block) {
     }
   }
 
+  // Ensure all blocks have at least one successor, except the Exit block.
+  if (block->GetSuccessors().empty() && !block->IsExitBlock()) {
+    AddError(StringPrintf("Block %d has no successor and it is not the Exit block.",
+                          block->GetBlockId()));
+  }
+
   // Ensure there is no critical edge (i.e., an edge connecting a
   // block with multiple successors to a block with multiple
   // predecessors). Exceptional edges are synthesized and hence