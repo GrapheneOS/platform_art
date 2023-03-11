hopez13/art

Don't check class-loader's liveness when sweeping interpreter/jit cache
Given the strong reference to the class-loader from class, it's
guaranteed that a live class will have its class-loader alive.
Therefore, there is no reason to check for class-loader's liveness when
sweeping interpreter/jit caches. Simply checking class' liveness
suffices.

Also, fix CMS' IsMarked() logic which earlier returned nullptr for
objects allocated after marking phase was completed. For such objects
check in the allocation stack.

Bug: 268689218
Bug: 160737021
Test: art/test/testrunner/testrunner.py --host
Change-Id: Ic845c0fc730bf4eb2c6b79e1b2cb10b53d22cac8

runtime/jit/jit_code_cache.cc
@@ -422,24 +422,13 @@ void JitCodeCache::SweepRootTables(IsMarkedVisitor* visitor) {
     for (uint32_t i = 0; i < number_of_roots; ++i) {
       // This does not need a read barrier because this is called by GC.
       mirror::Object* object = roots[i].Read<kWithoutReadBarrier>();
-      if (object == nullptr || object == Runtime::GetWeakClassSentinel()) {
-        // entry got deleted in a previous sweep.
-      } else if (object->IsString<kDefaultVerifyFlags>()) {
+      if (object != nullptr && object != Runtime::GetWeakSentinel()) {
         mirror::Object* new_object = visitor->IsMarked(object);
-        // We know the string is marked because it's a strongly-interned string that
-        // is always alive. The IsMarked implementation of the CMS collector returns
-        // null for newly allocated objects, but we know those haven't moved. Therefore,
-        // only update the entry if we get a different non-null string.
-        // TODO: Do not use IsMarked for j.l.Class, and adjust once we move this method
-        // out of the weak access/creation pause. b/32167580
-        if (new_object != nullptr && new_object != object) {
+        if (new_object == nullptr) {
+          roots[i] = GcRoot<mirror::Object>(Runtime::GetWeakSentinel());
+        } else if (new_object != object) {
           roots[i] = GcRoot<mirror::Object>(new_object);
         }
-      } else {
-        Runtime::ProcessWeakClass(
-            reinterpret_cast<GcRoot<mirror::Class>*>(&roots[i]),
-            visitor,
-            Runtime::GetWeakClassSentinel());
       }
     }
   }
@@ -449,7 +438,13 @@ void JitCodeCache::SweepRootTables(IsMarkedVisitor* visitor) {
     for (size_t i = 0; i < info->number_of_inline_caches_; ++i) {
       InlineCache* cache = &info->cache_[i];
       for (size_t j = 0; j < InlineCache::kIndividualCacheSize; ++j) {
-        Runtime::ProcessWeakClass(&cache->classes_[j], visitor, nullptr);
+        mirror::Class* klass = cache->classes_[j].Read<kWithoutReadBarrier>();
+        if (klass != nullptr) {
+          mirror::Class* new_klass = down_cast<mirror::Class*>(visitor->IsMarked(klass));
+          if (new_klass != klass) {
+            cache->classes_[j] = GcRoot<mirror::Class>(new_klass);
+          }
+        }
       }
     }
   }

GrapheneOS/platform_art

runtime/jit/jit_code_cache.cc
@@ -427,19 +427,20 @@ void JitCodeCache::SweepRootTables(IsMarkedVisitor* visitor) {
       } else if (object->IsString<kDefaultVerifyFlags>()) {
         mirror::Object* new_object = visitor->IsMarked(object);
         // We know the string is marked because it's a strongly-interned string that
-        // is always alive. The IsMarked implementation of the CMS collector returns
-        // null for newly allocated objects, but we know those haven't moved. Therefore,
-        // only update the entry if we get a different non-null string.
+        // is always alive.
         // TODO: Do not use IsMarked for j.l.Class, and adjust once we move this method
         // out of the weak access/creation pause. b/32167580
-        if (new_object != nullptr && new_object != object) {
+        DCHECK_NE(new_object, nullptr);
+        if (new_object != object) {
           roots[i] = GcRoot<mirror::Object>(new_object);
         }
       } else {
-        Runtime::ProcessWeakClass(
-            reinterpret_cast<GcRoot<mirror::Class>*>(&roots[i]),
-            visitor,
-            Runtime::GetWeakClassSentinel());
+        mirror::Object* new_klass = visitor->IsMarked(object);
+        if (new_klass == nullptr) {
+          roots[i] = GcRoot<mirror::Object>(Runtime::GetWeakClassSentinel());
+        } else if (new_klass != object) {
+          roots[i] = GcRoot<mirror::Object>(new_klass);
+        }
       }
     }
   }
@@ -449,7 +450,13 @@ void JitCodeCache::SweepRootTables(IsMarkedVisitor* visitor) {
     for (size_t i = 0; i < info->number_of_inline_caches_; ++i) {
       InlineCache* cache = &info->cache_[i];
       for (size_t j = 0; j < InlineCache::kIndividualCacheSize; ++j) {
-        Runtime::ProcessWeakClass(&cache->classes_[j], visitor, nullptr);
+        mirror::Class* klass = cache->classes_[j].Read<kWithoutReadBarrier>();
+        if (klass != nullptr) {
+          mirror::Class* new_klass = down_cast<mirror::Class*>(visitor->IsMarked(klass));
+          if (new_klass != klass) {
+            cache->classes_[j] = GcRoot<mirror::Class>(new_klass);
+          }
+        }
       }
     }
   }