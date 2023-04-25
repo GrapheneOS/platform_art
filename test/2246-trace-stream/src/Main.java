04c7c63a3694074640eaad54c61a4c4bcb02cb6b
Mythri Alle <mythria@google.com>
2023-04-25 14:57:35 +0000

    Update test to not trace thread start / join
    Thread start / join produce slightly non-deterministic output depending
    on the thread execution order. Calls to Thread.wait and Thread.isAlive
    were missing in some traces occasionally. This CL splits the tracing
    into two phases so that we can avoid tracing thread start / join for
    more deterministic output
    
    Bug: 259258187
    Test: art/test.py -t 2246
    Change-Id: I8c34832841293bec2921190229aa0a9aa1d7ba3f

--- test/2246-trace-stream/src/Main.java
+++ test/2246-trace-stream/src/Main.java
@@ -47,22 +47,38 @@ public class Main {
         FileOutputStream out_file = new FileOutputStream(file);
         Main m = new Main();
         Thread t = new Thread(() -> {
-            Main m1 = new Main();
-            m1.$noinline$doSomeWork();
+            try {
+                VMDebug.startMethodTracing(
+                        file.getPath(), out_file.getFD(), 0, 0, false, 0, streaming);
+                Main m1 = new Main();
+                m1.$noinline$doSomeWork();
+                VMDebug.$noinline$stopMethodTracing();
+                out_file.close();
+                parser.CheckTraceFileFormat(file, expected_version);
+                file.delete();
+            } catch (Exception e) {
+                System.out.println("Exception in thread " + e);
+                e.printStackTrace();
+            }
         }, "TestThread2246");
         try {
             if (VMDebug.getMethodTracingMode() != 0) {
                 VMDebug.$noinline$stopMethodTracing();
             }
 
-            VMDebug.startMethodTracing(file.getPath(), out_file.getFD(), 0, 0, false, 0, streaming);
             t.start();
             t.join();
+
+            file = createTempFile();
+            FileOutputStream main_out_file = new FileOutputStream(file);
+            VMDebug.startMethodTracing(
+                    file.getPath(), main_out_file.getFD(), 0, 0, false, 0, streaming);
             m.$noinline$doSomeWork();
             m.doSomeWorkThrow();
             VMDebug.$noinline$stopMethodTracing();
-            out_file.close();
+            main_out_file.close();
             parser.CheckTraceFileFormat(file, expected_version);
+            file.delete();
         } finally {
             if (out_file != null) {
                 out_file.close();
