hopez13/art

dexdump: Add dump string_ids capability (-s)
It can useful to inspect dex markers. These are contained in the
string_ids section but not referenced by anything.

This CL adds options -s to dump all strings in a dex file without
accounting if they are referenced or not.

Test: dexdump_test.cc
Bug: 283737440
Change-Id: If1b384638f3164728e2b90d6796e0397e536ad86

dexdump/dexdump.cc
@@ -1901,6 +1901,7 @@ static void dumpCallSite(const DexFile* pDexFile, u4 idx) {
   }
 }
 
+static void dumpStrings(const DexFile* p_file);
 /*
  * Dumps the requested sections of the file.
  */
@@ -1918,6 +1919,10 @@ static void processDexFile(const char* fileName,
   // Headers.
   if (gOptions.showFileHeaders) {
     dumpFileHeader(pDexFile);
+  }
+
+  if (gOptions.showAllStrings) {
+    dumpStrings(pDexFile);
   }
 
   // Iterate over all classes.
@@ -1942,6 +1947,34 @@ static void processDexFile(const char* fileName,
     fprintf(gOutFile, "</package>\n");
     free(package);
   }
+}
+
+static int isPrintable(const char* s) {
+  for (size_t i = 0; i < strlen(s); i++) {
+    if (!isprint((s[i]))) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/*
+ * Show all printable string in the string_ids section
+ */
+static void dumpStrings(const DexFile* dexFile) {
+  const DexFile::Header& pHeader = dexFile->GetHeader();
+  fprintf(gOutFile, "\nDisplaying %u strings from string_ids:\n", pHeader.string_ids_size_);
+
+  for (uint32_t i = 0; i < pHeader.string_ids_size_; i++) {
+    dex::StringIndex idx = static_cast<dex::StringIndex>(i);
+    const char* string = dexFile->StringDataByIdx(idx);
+    if (!isPrintable(string)) {
+      string = "skipped (not printable)";
+    }
+    fprintf(gOutFile, "  string[%06u] - '%s'\n", i, string);
+  }
+
+  fprintf(gOutFile, "\n");
 }
 
 /*

dexdump/dexdump_test.cc
@@ -67,8 +67,9 @@ TEST_F(DexDumpTest, BadFlagCombination) {
 
 TEST_F(DexDumpTest, FullPlainOutput) {
   std::string error_msg;
-  ASSERT_TRUE(Exec({"-d", "-f", "-h", "-l", "plain", "-o", "/dev/null",
-    dex_file_}, &error_msg)) << error_msg;
+  ASSERT_TRUE(
+      Exec({"-d", "-f", "-h", "-l", "-s", "plain", "-o", "/dev/null", dex_file_}, &error_msg))
+      << error_msg;
 }
 
 TEST_F(DexDumpTest, XMLOutput) {

GrapheneOS/platform_art

dexdump/dexdump.cc
@@ -1902,6 +1902,37 @@ static void dumpCallSite(const DexFile* pDexFile, u4 idx) {
 }
 
 /*
+ * Used to decide if we want to print or skip a string from string_ids
+ */
+static int isPrintable(const char* s) {
+  for (size_t i = 0; i < strlen(s); i++) {
+    if (!isprint((s[i]))) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/*
+ * Show all printable string in the string_ids section
+ */
+static void dumpStrings(const DexFile* pDexFile) {
+  const DexFile::Header& pHeader = pDexFile->GetHeader();
+  fprintf(gOutFile, "\nDisplaying %u strings from string_ids:\n", pHeader.string_ids_size_);
+
+  for (uint32_t i = 0; i < pHeader.string_ids_size_; i++) {
+    dex::StringIndex idx = static_cast<dex::StringIndex>(i);
+    const char* string = pDexFile->StringDataByIdx(idx);
+    if (!isPrintable(string)) {
+      string = "skipped (not printable)";
+    }
+    fprintf(gOutFile, "  string[%06u] - '%s'\n", i, string);
+  }
+
+  fprintf(gOutFile, "\n");
+}
+
+/*
  * Dumps the requested sections of the file.
  */
 static void processDexFile(const char* fileName,
@@ -1918,6 +1949,11 @@ static void processDexFile(const char* fileName,
   // Headers.
   if (gOptions.showFileHeaders) {
     dumpFileHeader(pDexFile);
+  }
+
+  // Strings.
+  if (gOptions.showAllStrings) {
+    dumpStrings(pDexFile);
   }
 
   // Iterate over all classes.

dexdump/dexdump_test.cc
@@ -67,8 +67,9 @@ TEST_F(DexDumpTest, BadFlagCombination) {
 
 TEST_F(DexDumpTest, FullPlainOutput) {
   std::string error_msg;
-  ASSERT_TRUE(Exec({"-d", "-f", "-h", "-l", "plain", "-o", "/dev/null",
-    dex_file_}, &error_msg)) << error_msg;
+  ASSERT_TRUE(
+      Exec({"-d", "-f", "-h", "-s", "-l", "plain", "-o", "/dev/null", dex_file_}, &error_msg))
+      << error_msg;
 }
 
 TEST_F(DexDumpTest, XMLOutput) {