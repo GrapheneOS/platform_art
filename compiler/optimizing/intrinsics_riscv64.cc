303d061d79ffd06aec960a921012712f49df844e
Vladimir Marko <vmarko@google.com>
2023-08-09 14:22:47 +0000

    riscv64: Add `intrinsics_riscv64.cc`.
    Test: m test-art-host-gtest
    Bug: 283082089
    Change-Id: I2b66387e7edec85af68d02dc7133a37cf33d46ff

---
+++ compiler/optimizing/intrinsics_riscv64.cc
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "intrinsics_riscv64.h"
+
+#include "code_generator_riscv64.h"
+
+namespace art HIDDEN {
+namespace riscv64 {
+
+#define MARK_UNIMPLEMENTED(Name) UNIMPLEMENTED_INTRINSIC(RISCV64, Name)
+UNIMPLEMENTED_INTRINSIC_LIST_RISCV64(MARK_UNIMPLEMENTED);
+#undef MARK_UNIMPLEMENTED
+
+UNREACHABLE_INTRINSICS(RISCV64)
+
+}  // namespace riscv64
+}  // namespace art
