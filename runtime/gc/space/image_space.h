hopez13/art

Cleanup: Use File instead of file descriptor for boot class path
Test: ./art/test.py -b --host --64
Change-Id: I734b156389486b7593c049915739dc9fbf8ea30f

runtime/gc/space/image_space.h
@@ -135,10 +135,10 @@ class ImageSpace : public MemMapSpace {
   //           extension is compiled only against the primary boot image.
   static bool LoadBootImage(const std::vector<std::string>& boot_class_path,
                             const std::vector<std::string>& boot_class_path_locations,
-                            const std::vector<int>& boot_class_path_fds,
-                            const std::vector<int>& boot_class_path_image_fds,
-                            const std::vector<int>& boot_class_path_vdex_fds,
-                            const std::vector<int>& boot_class_path_oat_fds,
+                            ArrayRef<File> boot_class_path_files,
+                            ArrayRef<File> boot_class_path_image_files,
+                            ArrayRef<File> boot_class_path_vdex_files,
+                            ArrayRef<File> boot_class_path_oat_files,
                             const std::vector<std::string>& image_locations,
                             const InstructionSet image_isa,
                             bool relocate,
@@ -290,7 +290,7 @@ class ImageSpace : public MemMapSpace {
   static bool ValidateOatFile(const OatFile& oat_file,
                               std::string* error_msg,
                               ArrayRef<const std::string> dex_filenames,
-                              ArrayRef<const int> dex_fds,
+                              ArrayRef<File> dex_fds,
                               const std::string& apex_versions);
 
   // Return the end of the image which includes non-heap objects such as ArtMethods and ArtFields.
@@ -347,18 +347,18 @@ class ImageSpace : public MemMapSpace {
     BootImageLayout(ArrayRef<const std::string> image_locations,
                     ArrayRef<const std::string> boot_class_path,
                     ArrayRef<const std::string> boot_class_path_locations,
-                    ArrayRef<const int> boot_class_path_fds,
-                    ArrayRef<const int> boot_class_path_image_fds,
-                    ArrayRef<const int> boot_class_path_vdex_fds,
-                    ArrayRef<const int> boot_class_path_oat_fds,
+                    ArrayRef<File> boot_class_path_files,
+                    ArrayRef<File> boot_class_path_image_files,
+                    ArrayRef<File> boot_class_path_vdex_files,
+                    ArrayRef<File> boot_class_path_oat_files,
                     const std::string* apex_versions)
         : image_locations_(image_locations),
           boot_class_path_(boot_class_path),
           boot_class_path_locations_(boot_class_path_locations),
-          boot_class_path_fds_(boot_class_path_fds),
-          boot_class_path_image_fds_(boot_class_path_image_fds),
-          boot_class_path_vdex_fds_(boot_class_path_vdex_fds),
-          boot_class_path_oat_fds_(boot_class_path_oat_fds),
+          boot_class_path_files_(boot_class_path_files),
+          boot_class_path_image_files_(boot_class_path_image_files),
+          boot_class_path_vdex_files_(boot_class_path_vdex_files),
+          boot_class_path_oat_files_(boot_class_path_oat_files),
           apex_versions_(*apex_versions) {}
 
     std::string GetPrimaryImageLocation();
@@ -459,10 +459,10 @@ class ImageSpace : public MemMapSpace {
     ArrayRef<const std::string> image_locations_;
     ArrayRef<const std::string> boot_class_path_;
     ArrayRef<const std::string> boot_class_path_locations_;
-    ArrayRef<const int> boot_class_path_fds_;
-    ArrayRef<const int> boot_class_path_image_fds_;
-    ArrayRef<const int> boot_class_path_vdex_fds_;
-    ArrayRef<const int> boot_class_path_oat_fds_;
+    ArrayRef<File> boot_class_path_files_;
+    ArrayRef<File> boot_class_path_image_files_;
+    ArrayRef<File> boot_class_path_vdex_files_;
+    ArrayRef<File> boot_class_path_oat_files_;
 
     std::vector<ImageChunk> chunks_;
     uint32_t base_address_ = 0u;

GrapheneOS/platform_art

runtime/gc/space/image_space.h
@@ -135,10 +135,10 @@ class ImageSpace : public MemMapSpace {
   //           extension is compiled only against the primary boot image.
   static bool LoadBootImage(const std::vector<std::string>& boot_class_path,
                             const std::vector<std::string>& boot_class_path_locations,
-                            const std::vector<int>& boot_class_path_fds,
-                            const std::vector<int>& boot_class_path_image_fds,
-                            const std::vector<int>& boot_class_path_vdex_fds,
-                            const std::vector<int>& boot_class_path_oat_fds,
+                            ArrayRef<File> boot_class_path_files,
+                            ArrayRef<File> boot_class_path_image_files,
+                            ArrayRef<File> boot_class_path_vdex_files,
+                            ArrayRef<File> boot_class_path_oat_files,
                             const std::vector<std::string>& image_locations,
                             const InstructionSet image_isa,
                             bool relocate,
@@ -290,7 +290,7 @@ class ImageSpace : public MemMapSpace {
   static bool ValidateOatFile(const OatFile& oat_file,
                               std::string* error_msg,
                               ArrayRef<const std::string> dex_filenames,
-                              ArrayRef<const int> dex_fds,
+                              ArrayRef<File> dex_files,
                               const std::string& apex_versions);
 
   // Return the end of the image which includes non-heap objects such as ArtMethods and ArtFields.
@@ -347,18 +347,18 @@ class ImageSpace : public MemMapSpace {
     BootImageLayout(ArrayRef<const std::string> image_locations,
                     ArrayRef<const std::string> boot_class_path,
                     ArrayRef<const std::string> boot_class_path_locations,
-                    ArrayRef<const int> boot_class_path_fds,
-                    ArrayRef<const int> boot_class_path_image_fds,
-                    ArrayRef<const int> boot_class_path_vdex_fds,
-                    ArrayRef<const int> boot_class_path_oat_fds,
+                    ArrayRef<File> boot_class_path_files,
+                    ArrayRef<File> boot_class_path_image_files,
+                    ArrayRef<File> boot_class_path_vdex_files,
+                    ArrayRef<File> boot_class_path_oat_files,
                     const std::string* apex_versions)
         : image_locations_(image_locations),
           boot_class_path_(boot_class_path),
           boot_class_path_locations_(boot_class_path_locations),
-          boot_class_path_fds_(boot_class_path_fds),
-          boot_class_path_image_fds_(boot_class_path_image_fds),
-          boot_class_path_vdex_fds_(boot_class_path_vdex_fds),
-          boot_class_path_oat_fds_(boot_class_path_oat_fds),
+          boot_class_path_files_(boot_class_path_files),
+          boot_class_path_image_files_(boot_class_path_image_files),
+          boot_class_path_vdex_files_(boot_class_path_vdex_files),
+          boot_class_path_oat_files_(boot_class_path_oat_files),
           apex_versions_(*apex_versions) {}
 
     std::string GetPrimaryImageLocation();
@@ -459,10 +459,10 @@ class ImageSpace : public MemMapSpace {
     ArrayRef<const std::string> image_locations_;
     ArrayRef<const std::string> boot_class_path_;
     ArrayRef<const std::string> boot_class_path_locations_;
-    ArrayRef<const int> boot_class_path_fds_;
-    ArrayRef<const int> boot_class_path_image_fds_;
-    ArrayRef<const int> boot_class_path_vdex_fds_;
-    ArrayRef<const int> boot_class_path_oat_fds_;
+    ArrayRef<File> boot_class_path_files_;
+    ArrayRef<File> boot_class_path_image_files_;
+    ArrayRef<File> boot_class_path_vdex_files_;
+    ArrayRef<File> boot_class_path_oat_files_;
 
     std::vector<ImageChunk> chunks_;
     uint32_t base_address_ = 0u;