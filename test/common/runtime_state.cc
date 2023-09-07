8aa54bd205ae14d523f243bfe6892c65d8e65527
Jiakai Zhang <jiakaiz@google.com>
2023-09-07 13:42:18 +0000

    Don't update the compiler filter when using a runtime image.
    Before this change, the runtime reports the compiler filter being
    "speed-profile" when using a runtime image. This was originally for
    reporting the app startup metric under a unique pair of dimension
    values (speed-profile, vdex).
    
    However, it turns out that the code that reports the app startup metric
    doesn't get the compiler filter from the runtime. Besides, the compiler
    filter change can have surprising side-effects. For example, when an app
    gets its own optimization status from the Java API
    `ApplicationRuntime.getBaseApkOptimizationInfo()`, it may find itself
    being in (speed-profile, install) even though its baseline profile is
    not used during app install.
    
    Bug: 260557058
    Change-Id: I46eb5015f2a382f5363b2bfab15be8c6a86a2f73
    Test: Presubmit

--- test/common/runtime_state.cc
+++ test/common/runtime_state.cc
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-#include "jni.h"
-
 #include <android-base/logging.h>
 #include <android-base/macros.h>
 #include <sys/resource.h>
@@ -25,10 +23,12 @@
 #include "base/enums.h"
 #include "common_throws.h"
 #include "dex/dex_file-inl.h"
+#include "gc/heap.h"
 #include "instrumentation.h"
 #include "jit/jit.h"
 #include "jit/jit_code_cache.h"
 #include "jit/profiling_info.h"
+#include "jni.h"
 #include "jni/jni_internal.h"
 #include "mirror/class-inl.h"
 #include "mirror/class.h"
@@ -467,6 +467,20 @@ extern "C" JNIEXPORT void JNICALL Java_Main_setAsyncExceptionsThrown([[maybe_unu
 extern "C" JNIEXPORT void JNICALL Java_Main_setRlimitNoFile(JNIEnv*, jclass, jint value) {
   rlimit limit { static_cast<rlim_t>(value), static_cast<rlim_t>(value) };
   setrlimit(RLIMIT_NOFILE, &limit);
+}
+
+extern "C" JNIEXPORT jboolean JNICALL Java_Main_isInImageSpace(JNIEnv* env,
+                                                               [[maybe_unused]] jclass caller,
+                                                               jclass cls) {
+  ScopedObjectAccess soa(env);
+
+  ObjPtr<mirror::Class> klass = soa.Decode<mirror::Class>(cls);
+  gc::space::Space* space =
+      Runtime::Current()->GetHeap()->FindSpaceFromObject(klass, /*fail_ok=*/true);
+  if (space == nullptr) {
+    return JNI_FALSE;
+  }
+  return space->IsImageSpace() ? JNI_TRUE : JNI_FALSE;
 }
 
 }  // namespace art
