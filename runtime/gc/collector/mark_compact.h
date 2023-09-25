98e883dbc17c40dd95eea01e5014bb2cacec4cca
Lokesh Gidra <lokeshgidra@google.com>
2023-09-25 22:38:12 +0000

    Use cached values for moving-space avoiding dependent loads
    For a given reference, GC needs to identify if it belongs to moving
    space or not. This is done for every reference on the heap and therefore
    is performance critical.
    
    This CL uses cached values for the moving-space range
    which would hopefully help the compiler optimize the checks.
    
    Bug: 160737021
    Test: manual
    Change-Id: I384fd5dd0da45fb4e7556647e0a64524188ee3c4

--- runtime/gc/collector/mark_compact.h
+++ runtime/gc/collector/mark_compact.h
@@ -136,7 +136,7 @@ class MarkCompact final : public GarbageCollector {
 
   mirror::Object* GetFromSpaceAddrFromBarrier(mirror::Object* old_ref) {
     CHECK(compacting_);
-    if (live_words_bitmap_->HasAddress(old_ref)) {
+    if (HasAddress(old_ref)) {
       return GetFromSpaceAddr(old_ref);
     }
     return old_ref;
@@ -240,11 +240,19 @@ class MarkCompact final : public GarbageCollector {
     }
   };
 
+  static bool HasAddress(mirror::Object* obj, uint8_t* begin, uint8_t* end) {
+    uint8_t* ptr = reinterpret_cast<uint8_t*>(obj);
+    return ptr >= begin && ptr < end;
+  }
+
+  bool HasAddress(mirror::Object* obj) const {
+    return HasAddress(obj, moving_space_begin_, moving_space_end_);
+  }
   // For a given object address in pre-compact space, return the corresponding
   // address in the from-space, where heap pages are relocated in the compaction
   // pause.
   mirror::Object* GetFromSpaceAddr(mirror::Object* obj) const {
-    DCHECK(live_words_bitmap_->HasAddress(obj)) << " obj=" << obj;
+    DCHECK(HasAddress(obj)) << " obj=" << obj;
     return reinterpret_cast<mirror::Object*>(reinterpret_cast<uintptr_t>(obj)
                                              + from_space_slide_diff_);
   }
@@ -266,9 +274,11 @@ class MarkCompact final : public GarbageCollector {
       REQUIRES_SHARED(Locks::mutator_lock_)
       REQUIRES(!Locks::heap_bitmap_lock_);
   // Update the reference at given offset in the given object with post-compact
-  // address.
-  ALWAYS_INLINE void UpdateRef(mirror::Object* obj, MemberOffset offset)
-      REQUIRES_SHARED(Locks::mutator_lock_);
+  // address. [begin, end) is moving-space range.
+  ALWAYS_INLINE void UpdateRef(mirror::Object* obj,
+                               MemberOffset offset,
+                               uint8_t* begin,
+                               uint8_t* end) REQUIRES_SHARED(Locks::mutator_lock_);
 
   // Verify that the gc-root is updated only once. Returns false if the update
   // shouldn't be done.
@@ -276,16 +286,23 @@ class MarkCompact final : public GarbageCollector {
                                             mirror::Object* old_ref,
                                             const RootInfo& info)
       REQUIRES_SHARED(Locks::mutator_lock_);
-  // Update the given root with post-compact address.
+  // Update the given root with post-compact address. [begin, end) is
+  // moving-space range.
   ALWAYS_INLINE void UpdateRoot(mirror::CompressedReference<mirror::Object>* root,
+                                uint8_t* begin,
+                                uint8_t* end,
                                 const RootInfo& info = RootInfo(RootType::kRootUnknown))
       REQUIRES_SHARED(Locks::mutator_lock_);
   ALWAYS_INLINE void UpdateRoot(mirror::Object** root,
+                                uint8_t* begin,
+                                uint8_t* end,
                                 const RootInfo& info = RootInfo(RootType::kRootUnknown))
       REQUIRES_SHARED(Locks::mutator_lock_);
   // Given the pre-compact address, the function returns the post-compact
-  // address of the given object.
-  ALWAYS_INLINE mirror::Object* PostCompactAddress(mirror::Object* old_ref) const
+  // address of the given object. [begin, end) is moving-space range.
+  ALWAYS_INLINE mirror::Object* PostCompactAddress(mirror::Object* old_ref,
+                                                   uint8_t* begin,
+                                                   uint8_t* end) const
       REQUIRES_SHARED(Locks::mutator_lock_);
   // Compute post-compact address of an object in moving space. This function
   // assumes that old_ref is in moving space.
@@ -712,6 +729,11 @@ class MarkCompact final : public GarbageCollector {
   size_t black_page_count_;
 
   uint8_t* from_space_begin_;
+  // Cached values of moving-space range to optimize checking if reference
+  // belongs to moving-space or not. May get updated if and when heap is
+  // clamped.
+  uint8_t* const moving_space_begin_;
+  uint8_t* moving_space_end_;
   // moving-space's end pointer at the marking pause. All allocations beyond
   // this will be considered black in the current GC cycle. Aligned up to page
   // size.
