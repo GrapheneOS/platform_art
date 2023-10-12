65c29bdb61a05fa9e9e7a1aff8daf3bd3069f233
Jiakai Zhang <jiakaiz@google.com>
2023-10-12 15:20:16 +0000

    Set a system property at boot for the embeded profile feature.
    Feature flags are not yet supported in mainline modules. As a temporary
    workaround, we set system properties during boot from the module (using
    art_boot) to indicate what features this module has. Those system
    properties should be cleaned up once flags are supported.
    
    Bug: 308137424
    Test: Presubmit
    
    Change-Id: Ic767ca5049c332080c273820c69a240b4a343e6d

--- tools/art_boot.cc
+++ tools/art_boot.cc
@@ -22,6 +22,16 @@
 #include "android-base/logging.h"
 #include "android-base/properties.h"
 
+static void SetPropertyAndLog(const std::string& key,
+                              const std::string& value,
+                              const std::string& message = "") {
+  if (android::base::SetProperty(key, value)) {
+    LOG(INFO) << "Set property " << key << " to " << value << " " << message;
+  } else {
+    LOG(ERROR) << "Failed to set property " << key << " to " << value << " " << message;
+  }
+}
+
 // Copies the value of one system property to another if it isn't empty and
 // passes the predicate test_fn.
 static void CopyPropertyIf(const char* src, const char* dst, bool (*test_fn)(const std::string&)) {
@@ -31,11 +41,7 @@ static void CopyPropertyIf(const char* src, const char* dst, bool (*test_fn)(con
   } else if (!test_fn(prop)) {
     LOG(INFO) << "Property " << src << " has ignored value " << prop;
   } else {
-    if (android::base::SetProperty(dst, prop)) {
-      LOG(INFO) << "Set property " << dst << " to " << prop << " from " << src;
-    } else {
-      LOG(ERROR) << "Failed to set property " << dst << " to " << prop;
-    }
+    SetPropertyAndLog(dst, prop, std::string("from ") + src);
   }
 }
 
@@ -50,6 +56,13 @@ int main(int, char** argv) {
                  // If an OEM has set dalvik.vm.useartservice to false we
                  // shouldn't override it to true from the P/H property.
                  [](const std::string& prop) { return prop == "false"; });
+
+  // The following system properties are temporarily used as feature flags to indicate whether the
+  // module has a particular change or not.
+  // Note that they don't actually control the runtime behavior. Instead, they are only used for
+  // guarding tests.
+  // TODO(b/305000383): Clean these up.
+  SetPropertyAndLog("dalvik.vm.features.embedded_profile", "true");
 
   return 0;
 }
