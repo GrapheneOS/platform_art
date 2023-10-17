843505420b9947bb13ff50f864b9b3fa8ada1537
Jiakai Zhang <jiakaiz@google.com>
2023-10-17 11:39:33 +0100

    Add an API to ignore all profiles during dexopt.
    This is for implementing the "--ignore-dexopt-profile" option.
    
    Context: go/platform-support-for-baseline-profiles
    
    The logic in adjustCompilerFilter is changing a little for the system UI
    and launcher special cases.
    
    Bug: 257532944
    Test: atest ArtServiceTests
    Test: atest CtsCompilationTestCases
    (cherry picked from https://android-review.googlesource.com/q/commit:169f746e61e596d99d8356cb7942ef178fc2d9a3)
    Merged-In: I20d79dd193d6c5f05ac7027e0a6c9bc93b007cb8
    Change-Id: I20d79dd193d6c5f05ac7027e0a6c9bc93b007cb8

--- libartservice/service/javatests/com/android/server/art/PrimaryDexopterParameterizedTest.java
+++ libartservice/service/javatests/com/android/server/art/PrimaryDexopterParameterizedTest.java
@@ -156,6 +156,18 @@ public class PrimaryDexopterParameterizedTest extends PrimaryDexopterTestBase {
         params.mSkipIfStorageLow = true;
         list.add(params);
 
+        params = new Params();
+        params.mIgnoreProfile = true;
+        params.mRequestedCompilerFilter = "speed-profile";
+        params.mExpectedCompilerFilter = "verify";
+        list.add(params);
+
+        params = new Params();
+        params.mIgnoreProfile = true;
+        params.mRequestedCompilerFilter = "speed";
+        params.mExpectedCompilerFilter = "speed";
+        list.add(params);
+
         return list;
     }
 
@@ -195,6 +207,8 @@ public class PrimaryDexopterParameterizedTest extends PrimaryDexopterTestBase {
                                 ArtFlags.FLAG_SHOULD_DOWNGRADE)
                         .setFlags(mParams.mSkipIfStorageLow ? ArtFlags.FLAG_SKIP_IF_STORAGE_LOW : 0,
                                 ArtFlags.FLAG_SKIP_IF_STORAGE_LOW)
+                        .setFlags(mParams.mIgnoreProfile ? ArtFlags.FLAG_IGNORE_PROFILE : 0,
+                                ArtFlags.FLAG_IGNORE_PROFILE)
                         .build();
 
         mPrimaryDexopter =
@@ -316,6 +330,7 @@ public class PrimaryDexopterParameterizedTest extends PrimaryDexopterTestBase {
         public boolean mForce = false;
         public boolean mShouldDowngrade = false;
         public boolean mSkipIfStorageLow = false;
+        public boolean mIgnoreProfile = false;
 
         // System properties.
         public boolean mAlwaysDebuggable = false;
@@ -328,8 +343,8 @@ public class PrimaryDexopterParameterizedTest extends PrimaryDexopterTestBase {
         public boolean mExpectedIsHiddenApiPolicyEnabled = true;
 
         public String toString() {
-            return String.format("mIsInDalvikCache=%b,"
-                            + "mHiddenApiEnforcementPolicy=%d,"
+            return String.format("isInDalvikCache=%b,"
+                            + "hiddenApiEnforcementPolicy=%d,"
                             + "isVmSafeMode=%b,"
                             + "isDebuggable=%b,"
                             + "isSystemUi=%b,"
@@ -338,7 +353,8 @@ public class PrimaryDexopterParameterizedTest extends PrimaryDexopterTestBase {
                             + "requestedCompilerFilter=%s,"
                             + "force=%b,"
                             + "shouldDowngrade=%b,"
-                            + "mSkipIfStorageLow=%b,"
+                            + "skipIfStorageLow=%b,"
+                            + "ignoreProfile=%b,"
                             + "alwaysDebuggable=%b"
                             + " => "
                             + "targetCompilerFilter=%s,"
@@ -347,8 +363,8 @@ public class PrimaryDexopterParameterizedTest extends PrimaryDexopterTestBase {
                             + "expectedIsHiddenApiPolicyEnabled=%b",
                     mIsInDalvikCache, mHiddenApiEnforcementPolicy, mIsVmSafeMode, mIsDebuggable,
                     mIsSystemUi, mIsLauncher, mIsUseEmbeddedDex, mRequestedCompilerFilter, mForce,
-                    mShouldDowngrade, mSkipIfStorageLow, mAlwaysDebuggable, mExpectedCompilerFilter,
-                    mExpectedDexoptTrigger, mExpectedIsDebuggable,
+                    mShouldDowngrade, mSkipIfStorageLow, mIgnoreProfile, mAlwaysDebuggable,
+                    mExpectedCompilerFilter, mExpectedDexoptTrigger, mExpectedIsDebuggable,
                     mExpectedIsHiddenApiPolicyEnabled);
         }
     }
