3a8c1b2c20170db44294d36fa870b30c697f6f67
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-23 15:54:37 +0300

    report progress of the initial BackgroundDexoptJob to PackageManager

--- libartservice/service/java/com/android/server/art/BackgroundDexoptJob.java
+++ libartservice/service/java/com/android/server/art/BackgroundDexoptJob.java
@@ -128,7 +128,17 @@ public class BackgroundDexoptJob {
             throw new IllegalStateException("This job cannot be scheduled");
         }
 
-        start();
+        start().thenAcceptAsync(result -> {
+            Map<Integer, DexoptResult> dr = null;
+            long durationMs = 0L;
+            if (result instanceof CompletedResult r) {
+                dr = r.dexoptResultByPass();
+                for (Long v : r.durationMsByPass().values()) {
+                    durationMs += v.longValue();
+                }
+            }
+            mInjector.getPackageManagerLocal().onBgDexoptCompleted(dr, durationMs);
+        });
 
         if (SystemProperties.getBoolean("pm.dexopt.disable_bg_dexopt", false /* def */)) {
             Log.i(TAG, "Job is disabled by system property 'pm.dexopt.disable_bg_dexopt'");
