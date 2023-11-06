hopez13/art

Don't cast null to a type in the verifier.
It may then fail if the value is used with an unrelated type.

Test: 851-null-instanceof
Bug: 157913277
Change-Id: I331caaff0c551fa20d14029e6d185960a3e9de5d

test/851-null-instanceof/src/Main.java
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Main {
+
+  static class A {}
+
+  static class B extends A {
+    void foo() {}
+  }
+
+  public static void main(String[] args) {
+    B b1 = System.currentTimeMillis() == 0 ? null : new B();
+    if (b1 instanceof A) {
+      b1.foo();
+    }
+  }
+}

GrapheneOS/platform_art

test/851-null-instanceof/src/Main.java
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Main {
+
+  static class A {}
+
+  static class B extends A {
+    void foo() {}
+  }
+
+  public static void main(String[] args) {
+    B b = System.currentTimeMillis() == 0 ? null : new B();
+    if (b instanceof A) {
+      b.foo();
+    }
+  }
+}