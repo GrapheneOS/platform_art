hopez13/art

Introduce kMaxPageSize and kMinPageSize
This patch is part of the chain preparing for making kPageSize
non-constexpr in a future patch.

The introduced constants designate minimum and maximum page sizes
supported by the configuration with dynamically determined page size.

This also introduces ART_PAGE_SIZE_AGNOSTIC preprocessor definition
in preparation to supporting page size agnosticism. It isn't yet defined
anywhere, however there will be a subsequent patch defining it according
to the ctx.Config().PageSizeAgnostic() build system configuration.

Test: Same as for I5430741a8494b340ed7fd2d8692c41a59ad9c530.
      The whole patches chain was tested as a whole.
Co-authored-by: Branislav Rankov <branislav.rankov@arm.com>
Co-authored-by: Ruben Ayrapetyan <ruben.ayrapetyan@arm.com>
Change-Id: I2527b5eb0f4a86cc654f0a20eef46f378cb537dd

libartbase/base/globals.h
@@ -38,10 +38,24 @@ static constexpr size_t kStackAlignment = 16;
 // compile-time constant so the compiler can generate better code.
 static constexpr size_t kPageSize = 4096;
 
+#if defined(ART_PAGE_SIZE_AGNOSTIC)
+// Minimum supported page size.
+static constexpr size_t kMinPageSize = 4096;
+
+// Maximum supported page size.
+static constexpr size_t kMaxPageSize = 16384;
+#else
+// Minimum supported page size.
+static constexpr size_t kMinPageSize = 4096;
+
+// Maximum supported page size.
+static constexpr size_t kMaxPageSize = 4096;
+#endif
+
 // Targets can have different page size (eg. 4kB or 16kB). Because Art can crosscompile, it needs
 // to be able to generate OAT (ELF) files with alignment other than the host page size.
 // kElfSegmentAlignment needs to be equal to the largest page size supported.
-static constexpr size_t kElfSegmentAlignment = kPageSize;
+static constexpr size_t kElfSegmentAlignment = kMaxPageSize;
 
 // TODO: Kernels for arm and x86 in both, 32-bit and 64-bit modes use 512 entries per page-table
 // page. Find a way to confirm that in userspace.

GrapheneOS/platform_art

libartbase/base/globals.h
@@ -38,6 +38,17 @@ static constexpr size_t kStackAlignment = 16;
 // compile-time constant so the compiler can generate better code.
 static constexpr size_t kPageSize = 4096;
 
+// Minimum supported page size.
+static constexpr size_t kMinPageSize = 4096;
+
+#if defined(ART_PAGE_SIZE_AGNOSTIC)
+// Maximum supported page size.
+static constexpr size_t kMaxPageSize = 16384;
+#else
+// Maximum supported page size.
+static constexpr size_t kMaxPageSize = kMinPageSize;
+#endif
+
 // TODO: Kernels for arm and x86 in both, 32-bit and 64-bit modes use 512 entries per page-table
 // page. Find a way to confirm that in userspace.
 // Address range covered by 1 Page Middle Directory (PMD) entry in the page table