hopez13/art

Add support for dex containers (DEX v41).
Allow multiple dex files within single "container"
(either a zip entry or a plain on-disk dex file).

This allows sharing of string (and other) dex data,
since the offsets can point to shared data payload.

Bug: 266950186
Test: test.py -b --host
Change-Id: I4f5901fd2f26a5a9dba427eb48c0fa5ddb6243d8

dex2oat/dex2oat_test.cc
@@ -1320,6 +1320,10 @@ TEST_F(Dex2oatTest, LayoutSections) {
   ASSERT_EQ(oat_dex_files.size(), 1u);
   // Check that the code sections match what we expect.
   for (const OatDexFile* oat_dex : oat_dex_files) {
+    if (oat_dex->GetDexVersion() >= DexFile::kDexContainerVersion) {
+      continue;  // Compact dex isn't supported together with dex container.
+    }
+
     const DexLayoutSections* const sections = oat_dex->GetDexLayoutSections();
     // Testing of logging the sections.
     ASSERT_TRUE(sections != nullptr);
@@ -1433,6 +1437,10 @@ TEST_F(Dex2oatTest, GenerateCompactDex) {
   std::vector<std::unique_ptr<const CompactDexFile>> compact_dex_files;
   for (const OatDexFile* oat_dex : oat_dex_files) {
     std::unique_ptr<const DexFile> dex_file(oat_dex->OpenDexFile(&error_msg));
+    if (dex_file->IsDexContainer()) {
+      ASSERT_FALSE(dex_file->IsCompactDexFile());
+      continue;  // Compact dex isn't supported together with dex container.
+    }
     ASSERT_TRUE(dex_file != nullptr) << error_msg;
     ASSERT_TRUE(dex_file->IsCompactDexFile());
     compact_dex_files.push_back(
@@ -1995,7 +2003,7 @@ TEST_F(Dex2oatWithExpectedFilterTest, AppImageEmptyDex) {
   {
     MutateDexFile(temp_dex.GetFile(), GetTestDexFileName("StringLiterals"), [&](DexFile* dex) {
       // Modify the header to make the dex file valid but empty.
-      DexFile::Header* header = const_cast<DexFile::Header*>(&dex->GetHeader());
+      DexFile::HeaderV41* header = const_cast<DexFile::HeaderV41*>(&dex->GetHeaderV41());
       header->string_ids_size_ = 0;
       header->string_ids_off_ = 0;
       header->type_ids_size_ = 0;
@@ -2011,7 +2019,7 @@ TEST_F(Dex2oatWithExpectedFilterTest, AppImageEmptyDex) {
       ASSERT_GT(header->file_size_,
                 sizeof(*header) + sizeof(dex::MapList) + sizeof(dex::MapItem) * 2);
       // Move map list to be right after the header.
-      header->map_off_ = sizeof(DexFile::Header);
+      header->map_off_ = sizeof(DexFile::HeaderV41);
       dex::MapList* map_list = const_cast<dex::MapList*>(dex->GetMapList());
       map_list->list_[0].type_ = DexFile::kDexTypeHeaderItem;
       map_list->list_[0].size_ = 1u;
@@ -2020,8 +2028,17 @@ TEST_F(Dex2oatWithExpectedFilterTest, AppImageEmptyDex) {
       map_list->list_[1].size_ = 1u;
       map_list->list_[1].offset_ = header->map_off_;
       map_list->size_ = 2;
-      header->data_off_ = header->map_off_;
-      header->data_size_ = map_list->Size();
+      if (dex->IsDexContainer()) {
+        header->data_off_ = 0;
+        header->data_size_ = 0;
+        header->container_offset = 0;
+        header->container_size = header->file_size_;
+      } else {
+        header->data_off_ = header->map_off_;
+        header->data_size_ = map_list->Size();
+        header->container_offset = 0;
+        header->container_size = 0;
+      }
     });
   }
   std::unique_ptr<const DexFile> dex_file(OpenDexFile(temp_dex.GetFilename().c_str()));

oatdump/oatdump.cc
@@ -647,6 +647,11 @@ class OatDumper {
         CHECK(oat_dex_file != nullptr);
         CHECK(vdex_dex_file != nullptr);
 
+        if (vdex_dex_file->IsDexContainerSecondary()) {
+          // All the data was already exported together with the primary dex file.
+          continue;
+        }
+
         // If a CompactDex file is detected within a Vdex container, DexLayout is used to convert
         // back to a StandardDex file. Since the converted DexFile will most likely not reproduce
         // the original input Dex file, the `update_checksum_` option is used to recompute the
@@ -992,6 +997,11 @@ class OatDumper {
           os << "Unexpected checksum from unquicken dex file '" << dex_file_location << "'\n";
           return false;
         }
+      }
+      if (dex_file->IsDexContainer()) {
+        // Extend the data range to export all the dex files in the container.
+        DCHECK(dex_file->IsDexContainerPrimary()) << dex_file_location;
+        fsize = dex_file->GetHeaderV41().container_size;
       }
     }
 

runtime/dex2oat_environment_test.h
@@ -131,8 +131,14 @@ class Dex2oatEnvironmentTest : public Dex2oatScratchDirs, public CommonRuntimeTe
     ASSERT_EQ(multi1[0]->GetHeader().checksum_, multi2[0]->GetHeader().checksum_);
     ASSERT_NE(multi1[1]->GetHeader().checksum_, multi2[1]->GetHeader().checksum_);
 
-    ASSERT_EQ(multi1[0]->GetLocationChecksum(), multi2[0]->GetLocationChecksum());
-    ASSERT_NE(multi1[1]->GetLocationChecksum(), multi2[1]->GetLocationChecksum());
+    if (multi1[0]->IsDexContainer()) {
+      // Checksum is the CRC of the whole container, so both of them should differ.
+      ASSERT_NE(multi1[0]->GetLocationChecksum(), multi2[0]->GetLocationChecksum());
+      ASSERT_NE(multi1[1]->GetLocationChecksum(), multi2[1]->GetLocationChecksum());
+    } else {
+      ASSERT_EQ(multi1[0]->GetLocationChecksum(), multi2[0]->GetLocationChecksum());
+      ASSERT_NE(multi1[1]->GetLocationChecksum(), multi2[1]->GetLocationChecksum());
+    }
   }
 
   void SetUpRuntimeOptions(RuntimeOptions* options) override {

GrapheneOS/platform_art

dex2oat/dex2oat_test.cc
@@ -1320,6 +1320,10 @@ TEST_F(Dex2oatTest, LayoutSections) {
   ASSERT_EQ(oat_dex_files.size(), 1u);
   // Check that the code sections match what we expect.
   for (const OatDexFile* oat_dex : oat_dex_files) {
+    if (oat_dex->GetDexVersion() >= DexFile::kDexContainerVersion) {
+      continue;  // Compact dex isn't supported together with dex container.
+    }
+
     const DexLayoutSections* const sections = oat_dex->GetDexLayoutSections();
     // Testing of logging the sections.
     ASSERT_TRUE(sections != nullptr);
@@ -1433,6 +1437,10 @@ TEST_F(Dex2oatTest, GenerateCompactDex) {
   std::vector<std::unique_ptr<const CompactDexFile>> compact_dex_files;
   for (const OatDexFile* oat_dex : oat_dex_files) {
     std::unique_ptr<const DexFile> dex_file(oat_dex->OpenDexFile(&error_msg));
+    if (dex_file->HasDexContainer()) {
+      ASSERT_FALSE(dex_file->IsCompactDexFile());
+      continue;  // Compact dex isn't supported together with dex container.
+    }
     ASSERT_TRUE(dex_file != nullptr) << error_msg;
     ASSERT_TRUE(dex_file->IsCompactDexFile());
     compact_dex_files.push_back(
@@ -2011,7 +2019,7 @@ TEST_F(Dex2oatWithExpectedFilterTest, AppImageEmptyDex) {
       ASSERT_GT(header->file_size_,
                 sizeof(*header) + sizeof(dex::MapList) + sizeof(dex::MapItem) * 2);
       // Move map list to be right after the header.
-      header->map_off_ = sizeof(DexFile::Header);
+      header->map_off_ = header->header_size_;
       dex::MapList* map_list = const_cast<dex::MapList*>(dex->GetMapList());
       map_list->list_[0].type_ = DexFile::kDexTypeHeaderItem;
       map_list->list_[0].size_ = 1u;
@@ -2022,6 +2030,7 @@ TEST_F(Dex2oatWithExpectedFilterTest, AppImageEmptyDex) {
       map_list->size_ = 2;
       header->data_off_ = header->map_off_;
       header->data_size_ = map_list->Size();
+      header->SetDexContainer(0, header->file_size_);
     });
   }
   std::unique_ptr<const DexFile> dex_file(OpenDexFile(temp_dex.GetFilename().c_str()));

oatdump/oatdump.cc
@@ -647,6 +647,11 @@ class OatDumper {
         CHECK(oat_dex_file != nullptr);
         CHECK(vdex_dex_file != nullptr);
 
+        if (!vdex_dex_file->IsDexContainerFirstEntry()) {
+          // All the data was already exported together with the primary dex file.
+          continue;
+        }
+
         // If a CompactDex file is detected within a Vdex container, DexLayout is used to convert
         // back to a StandardDex file. Since the converted DexFile will most likely not reproduce
         // the original input Dex file, the `update_checksum_` option is used to recompute the
@@ -993,6 +998,9 @@ class OatDumper {
           return false;
         }
       }
+      // Extend the data range to export all the dex files in the container.
+      CHECK(dex_file->IsDexContainerFirstEntry()) << dex_file_location;
+      fsize = dex_file->GetHeader().ContainerSize();
     }
 
     // Verify output directory exists

runtime/dex2oat_environment_test.h
@@ -131,8 +131,14 @@ class Dex2oatEnvironmentTest : public Dex2oatScratchDirs, public CommonRuntimeTe
     ASSERT_EQ(multi1[0]->GetHeader().checksum_, multi2[0]->GetHeader().checksum_);
     ASSERT_NE(multi1[1]->GetHeader().checksum_, multi2[1]->GetHeader().checksum_);
 
-    ASSERT_EQ(multi1[0]->GetLocationChecksum(), multi2[0]->GetLocationChecksum());
-    ASSERT_NE(multi1[1]->GetLocationChecksum(), multi2[1]->GetLocationChecksum());
+    if (multi1[0]->HasDexContainer()) {
+      // Checksum is the CRC of the whole container, so both of them should differ.
+      ASSERT_NE(multi1[0]->GetLocationChecksum(), multi2[0]->GetLocationChecksum());
+      ASSERT_NE(multi1[1]->GetLocationChecksum(), multi2[1]->GetLocationChecksum());
+    } else {
+      ASSERT_EQ(multi1[0]->GetLocationChecksum(), multi2[0]->GetLocationChecksum());
+      ASSERT_NE(multi1[1]->GetLocationChecksum(), multi2[1]->GetLocationChecksum());
+    }
   }
 
   void SetUpRuntimeOptions(RuntimeOptions* options) override {