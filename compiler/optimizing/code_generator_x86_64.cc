04ccad00ea3b95a2477de4cc418ad55a92b5b5b4
Vladimir Marko <vmarko@google.com>
2023-12-01 16:35:20 +0100

    x86-64: Implement Unsafe.getAndAdd/-Set intrinsics.
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing
    Bug: 26264765
    Bug: 202868177
    Change-Id: I1c7127cf970055dcc176f274d9c2f32c4ed77ac7

--- compiler/optimizing/code_generator_x86_64.cc
+++ compiler/optimizing/code_generator_x86_64.cc
@@ -620,7 +620,9 @@ class ReadBarrierMarkAndUpdateFieldSlowPathX86_64 : public SlowPathCode {
         << "Unexpected instruction in read barrier marking and field updating slow path: "
         << instruction_->DebugName();
     HInvoke* invoke = instruction_->AsInvoke();
-    DCHECK(IsUnsafeCASReference(invoke) || IsVarHandleCASFamily(invoke)) << invoke->GetIntrinsic();
+    DCHECK(IsUnsafeCASReference(invoke) ||
+           IsUnsafeGetAndSetReference(invoke) ||
+           IsVarHandleCASFamily(invoke)) << invoke->GetIntrinsic();
 
     __ Bind(GetEntryLabel());
     if (unpoison_ref_before_marking_) {
