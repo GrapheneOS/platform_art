854cb7d94594f027cf0f056d6cd023e7a00df0cd
Roland Levillain <rpl@google.com>
2023-12-06 18:38:36 +0000

    Enable `userfaultfd`-based CMC GC by default on Android S and above.
    Before this change, the Concurrent Mark-Compact Garbage Collector
    (CMC GC) was enabled by default (provided the kernel supports
    `userfaultfd`) on Android T and above. Extend this behavior to
    Android S and above.
    
    This reverts commit a9102c122084ced4a5369d2c2174ea1b83b4e1c9.
    
    Change-Id: I378fe28b657704031501c669beddec4afa4838c4
    Test: atest art_standalone_odrefresh_tests
    Test: atest odsign_e2e_tests odsign_e2e_tests_full
    Test: Build bundled module on main with OVERRIDE_ENABLE_UFFD_GC=false,
      check that odrefresh runs on boot, and check that logcat shows
      "Using CollectorTypeCMC GC."
    Test: Build module on Mainline branch, install on device running S,
      check that odrefresh runs on reboot, and check that logcat shows
      "Using CollectorTypeCMC GC."
      Reboot again, and check that odrefresh does not run and that logcat
      shows "Using CollectorTypeCMC GC."
    Test: Same as above on a device running T, and then:
          adb root
          adb shell device_config set_sync_disabled_for_tests persistent
          adb shell setprop persist.device_config.runtime_native_boot.force_disable_uffd_gc true
          adb reboot
      Check that odrefresh runs on reboot, and check that logcat shows
      "Using CollectorTypeCC GC."
      Reboot again, and check that odrefresh does not run and that logcat
      shows "Using CollectorTypeCC GC."
    Bug: 251150519

--- odrefresh/odr_common.cc
+++ odrefresh/odr_common.cc
@@ -88,10 +88,8 @@ void SystemPropertyForeach(std::function<void(const char* name, const char* valu
       &action);
 }
 
-bool CheckBuildUserfaultFdGc(bool build_enable_uffd_gc,
-                             bool is_at_least_t,
-                             bool kernel_supports_uffd) {
-  bool runtime_uses_uffd_gc = (build_enable_uffd_gc || is_at_least_t) && kernel_supports_uffd;
+bool CheckBuildUserfaultFdGc(bool build_enable_uffd_gc, bool kernel_supports_uffd) {
+  bool runtime_uses_uffd_gc = kernel_supports_uffd;
   return build_enable_uffd_gc == runtime_uses_uffd_gc;
 }
 
