7be9bb21053f933f639f0446c8060372d66b2ac6
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-12-29 22:30:10 +0200

    use speed-profile compilerFilter for pre-reboot dexopt of 3rd party apps

--- libartservice/service/java/com/android/server/art/ReasonMapping.java
+++ libartservice/service/java/com/android/server/art/ReasonMapping.java
@@ -175,6 +175,10 @@ public class ReasonMapping {
      */
     @NonNull
     public static String getCompilerFilterForReason(@NonNull String reason) {
+        if (REASON_PRE_REBOOT_DEXOPT.equals(reason)) {
+            return "speed-profile";
+        }
+
         String value = SystemProperties.get("pm.dexopt." + reason);
         if (TextUtils.isEmpty(value)) {
             if (reason.equals(REASON_POST_UNATTENDED_REBOOT)) {
